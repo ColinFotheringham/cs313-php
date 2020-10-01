ddea32c6df4c81ea00b39b0a399b0b1fd9f9bded
ColinFotheringham <colinfthrnghm@gmail.com>
2020-10-01 16:24:35 -0600

    updated

--- web/Team_Assignments/profile.php
+++ web/Team_Assignments/profile.php
@@ -1,3 +1,11 @@
+<?php
+
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$major = htmlspecialchars($_POST["radio"]);
+$comments = htmlspecialchars($_POST["comments"]);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,21 +15,19 @@
 </head>
 <body>
     
-        <h4>Name: </h4> 
+        <h4>Name: <?=$name ?> </h4> 
         
-        <p><?php 
-        htmlspecialchars(echo $_POST["name"]);?> </p>
      
         <h4>Email: </h4>
-        <a href="mailto:<?php htmlspecialchars( echo $_POST["email"]); ?>"> <?php htmlspecialchars(echo $_POST["email"]);?> </a> 
+        <a href="mailto:<?=$email ?>"> <?=$email ?> </a> 
 
-        <h4> Major </h4>
-        <?php htmlspecialchars(echo $_POST["radio"]);?>
-        <h4>comments</h4>
-        <?php htmlspecialchars(echo $_POST["comments"]);?>
+        <h4> Major <?=$major ?></h4>
+       
+        <h4>comments <?=$comments ?> </h4>
+     
        
         <h4> This user has visited:</h4>
-        <?php $acountry = htmlspecialchars (echo $_POST["continents"]);
+        <?php $acountry = htmlspecialchars(echo $_POST["continents"]);
         
         $N = count($acountry);
 
